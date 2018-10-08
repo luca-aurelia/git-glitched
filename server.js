843f8ab6c4ca916aa360f5da43a678b9a27c7bc6
Thomas Bailey <thomas@noisemachines.io>
2018-10-08 16:32:38 -0400

    Serve readme from root.

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-// server.js
-
 const express = require('express')
 const app = express()
 const { execSync } = require('child_process')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
-  response.status(200).send('おかえり！🏡')
+  response.sendFile(path.join(__dirname, 'readme.md'))
 })
 
 app.post('/deploy', (request, response) => {
@@ -34,7 +33,6 @@ app.post('/deploy', (request, response) => {
   response.status(200).send()
 })
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port)
 })
