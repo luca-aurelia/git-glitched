c61f3724b0fcc34342ebb04da92b405ec83826fd
Thomas Bailey <thomas@noisemachines.io>
2018-10-08 13:16:12 -0400

    Tweak some stuff.

--- server.js
+++ server.js
@@ -4,19 +4,32 @@
 // init project
 const express = require('express')
 const app = express()
+const { execSync } = require('child_process')
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+app.post('/deploy', (request, response) => {
+  if (request.query.secret === process.env.SECRET) {
+    console.log(request)
+    response.send(200)
+  } else {
+    response.send(401)
+  }
 })
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
+
+const output = execSync('echo hello').toString()
+console.log({ output })
