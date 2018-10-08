8946278a2234e2c65049efb9db18cde323ad54b1
Thomas Bailey <thomas@noisemachines.io>
2018-10-08 13:27:22 -0400

    More tweaks.

--- server.js
+++ server.js
@@ -5,31 +5,39 @@
 const express = require('express')
 const app = express()
 const { execSync } = require('child_process')
+const bodyParser = require('body-parser')
 
-// we've started you off with Express,
+// we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
+  response.sendFile(__dirname + '/views/index.html');
 })
 
 app.post('/deploy', (request, response) => {
-  if (request.query.secret === process.env.SECRET) {
-    console.log(request)
-    response.send(200)
-  } else {
-    response.send(401)
+  if (request.query.secret !== process.env.SECRET) {
+    response.status(401).send()
+    return
   }
+  
+  if (request.body.ref !== 'refs/heads/glitch') {
+    response.status(200).send('Push was not to glitch branch, so did not deploy.')
+    return
+  }
+  
+  const output = execSync('git pull origin glitch; refresh').toString()
+  console.log({ output })
+  response.status(200).send()
 })
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 
-const output = execSync('echo hello').toString()
-console.log({ output })
+console.log('Updated 1:27 pm.')
